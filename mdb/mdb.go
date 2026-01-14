2a8eaebd9b453cd34ed312fa9a3a81c4fc6da855
Yohanes <yohanesdahlan@gmail.com>
2026-01-14 18:11:26 +0700

    CRUD

--- mdb/mdb.go
+++ mdb/mdb.go
@@ -57,3 +57,107 @@ func emailEntryFromRow(row *sql.Rows) (*EmailEntry, error) {
 		OptOut:      optOut,
 	}, nil
 }
+
+func CreateEmail(db *sql.DB, email string) error {
+	_, err := db.Exec(`
+		INSERT
+		INTO
+		emails(email, confirmed_at, opt_out)
+		VALUES(?, 0, false)`, email)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+// single email
+func GetEmail(db *sql.DB, email string) (*EmailEntry, error) {
+	rows, err := db.Query(`
+	SELECT id, email, confirmed_at, opt_out
+	FROM emails
+	WHERE email = ?`, email)
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		return emailEntryFromRow(rows)
+	}
+	return nil, nil
+}
+
+func UpdateEmail(db *sql.DB, entry EmailEntry) error {
+	t := entry.ConfirmedAt.Unix()
+
+	_, err := db.Exec(`
+	INSERT
+	INTO
+	emails(email, confirmed_at, opt_out)
+	VALUES(?, ?, ?)
+	ON CONFLICT(email) DO UPDATE SET
+		confirmed_at=?
+		opt_out=?`, entry.Email, t, entry.OptOut, t, entry.OptOut)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func DeleteEmail(db *sql.DB, email string) error {
+	_, err := db.Exec(`
+	UPDATE emails
+	SET opt_out=true
+	WHERE email=?`, email)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+type GetEmailBatchQueryParams struct {
+	Page  int
+	Count int
+}
+
+// many emails
+func GetEmailBatch(db *sql.DB, params GetEmailBatchQueryParams) ([]EmailEntry, error) {
+	var empty []EmailEntry
+
+	rows, err := db.Query(`
+	SELECT id, email, confirmed_at, opt_out
+	FROM emails
+	WHERE opt_out = false
+	ORDER BY id ASC
+	LIMIT ? OFFSET ?`, params.Count, (params.Page-1)*params.Count)
+
+	if err != nil {
+		log.Println(err)
+		return empty, err
+	}
+
+	defer rows.Close()
+
+	emails := make([]EmailEntry, 0, params.Count)
+
+	for rows.Next() {
+		email, err := emailEntryFromRow(rows)
+		if err != nil {
+			return nil, err
+		}
+		emails = append(emails, *email)
+	}
+
+	return emails, nil
+}
